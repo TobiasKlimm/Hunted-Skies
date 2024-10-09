3538167ef8ffa9ad82a76deeeaeb2a05f5f49798
TobiasKlimm <klimmtobias@gmail.com>
2024-10-09 15:57:56 +0200

    Setup

---
+++ VektoriaApp/Game.h
@@ -0,0 +1,78 @@
+#pragma once
+//-----------------------------------------------------------------------
+// CGame
+// 
+// Erklärung : Eine weitestgehend leere Klasse, 
+//             sinnfällig in eine Windows-Applikation eingebettet,
+//             um ein Spiel bzw. eine 3D-Simulation zu erzeugen
+// Autor     : Prof. Dr. Tobias Breiner
+// Ort       : Pfungstadt
+// Zeit      : seit Aug. 2011 (mit seitdem ständigen Verbesserungs-Updates)
+// Copyright : Tobias Breiner  
+// Disclaimer: Nutzung auf eigene Gefahr, keine Gewähr, no warranty!
+//------------------------------------------------------------------------
+
+
+#ifdef _WIN64
+#ifdef _DEBUG
+#pragma comment (lib, "Vektoria_Debug64.lib")
+#pragma comment (lib, "VektoriaMath_Debug64.lib")
+#else
+#pragma comment (lib, "Vektoria_Release64.lib")
+#pragma comment (lib, "VektoriaMath_Release64.lib")
+#endif
+#else
+#ifdef _DEBUG
+#pragma comment (lib, "Vektoria_Debug.lib")
+#pragma comment (lib, "VektoriaMath_Debug.lib")
+#else
+#pragma comment (lib, "Vektoria_Release.lib")
+#pragma comment (lib, "VektoriaMath_Release.lib")
+#endif
+#endif
+
+
+#include "Vektoria\Root.h"
+
+using namespace Vektoria;
+
+
+class CGame
+{
+public:
+	// Wird vor Begin einmal aufgerufen (Konstruktor):
+	CGame(void);																				
+
+	// Wird nach Ende einmal aufgerufen (Destruktor):
+	~CGame(void);																				
+
+	// Wird zu Begin einmal aufgerufen:
+	void Init(HWND hwnd, void(*procOS)(HWND hwnd, unsigned int uWndFlags), CSplash * psplash);	
+
+	// Wird während der Laufzeit bei jedem Bildaufbau aufgerufen:
+	void Tick(float fTime, float fTimeDelta);						
+
+	// Wird am Ende einmal aufgerufen:
+	void Fini();																				
+
+	// Wird immer dann aufgerufen, wenn der Benutzer die Fenstergröße verändert hat:
+	void WindowReSize(int iNewWidth, int iNewHeight);											
+
+	// Holt das minimale Zeitdelta zur eventuellen Frame-Rate-Beschränkung:
+	float GetTimeDeltaMin();																	
+
+	// Holt die Versionsnummer:
+	float GetVersion();
+
+
+private:
+
+	// Die Wurzel des Szenengrafen: 
+	CRoot m_zr; 
+
+	// Hier ist Platz für Deine weiteren Vektoriaobjekte:
+
+
+};
+
+
