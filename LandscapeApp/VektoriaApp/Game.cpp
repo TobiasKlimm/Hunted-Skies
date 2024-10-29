b201fb56e63dae30179417d9e5d69ad7f7e62511
Rim3Xx <fweiske@gmx.de>
2024-10-29 16:28:40 +0100

    Updatet landscape

--- LandscapeApp/VektoriaApp/Game.cpp
+++ LandscapeApp/VektoriaApp/Game.cpp
@@ -8,7 +8,7 @@ CGame::~CGame(void)
 {
 }
 
-void CGame::Init(HWND hwnd, void(*procOS)(HWND hwnd, unsigned int uWndFlags), CSplash * psplash)
+void CGame::Init(HWND hwnd, void(*procOS)(HWND hwnd, unsigned int uWndFlags), CSplash* psplash)
 {
 	//---------------------------------
 	// Initialisiere die Knotenobjekte:
@@ -16,9 +16,10 @@ void CGame::Init(HWND hwnd, void(*procOS)(HWND hwnd, unsigned int uWndFlags), CS
 
 	// Zuallererst muss die Root mit dem Splash-Screen initialisiert werden, 
 	// damit die Engine freigeschaltet wird:
-	m_zr.Init(psplash);
+
 
 	// Hier kommen alle weiteren Initialisierungen hinein: 
+	// Das kennen wir ja schon:
 	m_zr.Init(psplash);
 	m_zf.Init(hwnd, procOS);
 	m_zv.InitFull(&m_zc);
@@ -26,159 +27,157 @@ void CGame::Init(HWND hwnd, void(*procOS)(HWND hwnd, unsigned int uWndFlags), CS
 	m_zf.AddViewport(&m_zv);
 	m_zf.AddDeviceKeyboard(&m_zdk);
 	m_zr.AddScene(&m_zs);
+	m_zv.SetHazeOn();
 
+
+
+
+	m_pgtriangletable = m_filewavefront.LoadGeoTriangleTable("Models\\Cannon\\cannon_01_4k.obj");
+	m_zs.AddPlacement(&m_zpCannon);
+	m_zpCannon.AddGeo(m_pgtriangletable);
+
+	m_zpCannon.ScaleDelta(10.0f);
+	m_zpCannon.TranslateDelta(-300, 0, 700);
+
+
+
+	m_pgtriangletable1 = m_filewavefront1.LoadGeoTriangleTable("Models\\ship_pinnace_4k.blend\\ship_pinnace_4k.obj");
+	m_zs.AddPlacement(&m_zpShip);
+	m_zpShip.AddGeo(m_pgtriangletable1);
+
+	m_zpShip.ScaleDelta(10.0f);
+	m_zpShip.RotateYDelta(HALFPI);
+	m_zpShip.TranslateDelta(-350, 0, 1000);
 
 
 	// Erzeuge einen Himmel mit Sonne, Mond und Sternen:
 	m_zs.SetSkyOn(&m_zpCamera);
-	m_zs.SetSkyFlowOn(0);
+	m_zs.SetSkyTimeOfDay(0.0);
+	m_zs.SetSkyTimeOfYear(0.75);
+	m_zs.SetSkyFlowOn(1200);
+	m_zs.SetSkyMistOn(true);
+	m_zs.SetSkyMistStrength(0.00017f);
+	m_zs.SetSkyMistStartDistance(18000.00003f);
 
-	// Schalte den Haze-Postprocessing-Filter fürs Wasser an:
-	m_zv.SetHazeOn();
-
-	// Erzeuge einen Cut, der alles unter N.N. wegschneidet:
-	m_cutUnderSea.SetHeightLimits(-F_MAX, -100.0f);
-
-	// Die Vertices der den Rand überlappenden Polygone werden
-	// auf N.N. angehoben:
-	m_cutUnderSea.SetFlattenLowerOn();
-
-	// Und in der XZ-Ebene mittels Bézier „glattgezurrt“:
-	m_cutUnderSea.SetFlattenSmartOn();
-
-	// Füge den Cut dem Terrain hinzu:
-	m_zgTerrain.AddCut(&m_cutUnderSea);
-
-
-	// Lade die Texturen für Land:
-	m_zmEarth.LoadPreset("RockMossy");
-
-	// --------------------
-	// Erzeuge des Wassers:
-	// --------------------
-
-	// Lade die vorgefertigte Wassertextur aus der Lib:
-	m_zmWater.LoadPreset("Water");
-
-	// Die animierte Wassertextur hat 8*8 Bilder:
-	m_zmWater.SetAni(8, 8, 7);
-
-	// Erh�he den Blauwert (Bilde die Blaustreuung des Wassers nach):
-	m_zmWater.Translate(CColor(0.0f, 0.2f, 0.3f));
-
-	// Die Transparenz des Wassers ist 90%:
-	m_zmWater.SetTransparency(0.7f);
-
-	// Steuere den Haze-Filter, damit das Wasser "schwappt": 
-	m_zmWater.MakeTextureHaze("textures\\waterHaze.jpg");
-	m_zmWater.SetPostprocessingOn();
-	m_zmWater.SetHazeOn();
-	m_zmWater.SetHazeStrength(1.8);
-
-	// Erzeuge die Wasseroberfläche:
-	m_zgWater.Init(
-		70000, 70000, // Das Meer ist groß (70 *70 qkm)
-		&m_zmWater, // Ein bewegtes Wassermaterial mit Wellen
-		10, 10, // Brauchen nur wenige Polygone (hier 100)
-		0.0f, 0.0f, // Eine UV-Verschiebung brauchen wir nicht
-		500.0f, // Aber die Textur soll sich 500 Mal …
-		500.0f); // …in jeder Richtung wiederholen
-
-
-	// -----------------------
-	// Erzeuge die Landschaft:
-	// -----------------------
-	m_pperlin = new CPerlin(42, 3.0f, 15, 0.5f, 8.0f, 1.50f, 1.5f, ePerlinInterpol_Standard, false);
-
-	m_pblob = new CBlob(0.5f, 0.5f, 0.6f, 0.6f, -0.1f, eBlobShapeGround_Rect, eBlobShapeSide_All, NULL);
-	m_pblob2 = new CBlob(0.5f, 0.5f, 0.5f, 0.5f, 500, eBlobShapeGround_Radial, eBlobShapeSide_Hill, m_pperlin);
-	m_pblob3 = new CBlob(0.2f, 0.2f, 0.2f, 0.2f, 500, eBlobShapeGround_Donut, eBlobShapeSide_Hill, m_pperlin);
-
-	m_zgTerrainOriginal.AddBlob(m_pblob);
-	m_zgTerrainOriginal.AddBlob(m_pblob2);
-	m_zgTerrainOriginal.AddBlob(m_pblob3);
-	//Deine mutter stinkt
-
-	m_zgTerrainOriginal.CreateField(
-		2500, 2500, // Terraingröße
-		1000, 1000, // Vertices
-		0.0f, 0.0f, // UV Textur
-		1.0f, 1.0f); // UV Textur
-
-	// Erzeuge mit dem Terraindaten eine Terraingeometrie:
-	m_zgTerrain.InitFromOther(m_zgTerrainOriginal, &m_zmEarth);
-
-	// Das Blaupausen-Terrain soll nicht gerendert werden:
-	m_zgTerrainOriginal.SetDrawingOff();
 
 	// Hänge die Geometrien an ein geeignetes Placement:
 	m_zs.AddPlacement(&m_zpLandscape);
 	m_zpLandscape.AddGeo(&m_zgTerrain);
 	m_zpLandscape.AddGeo(&m_zgWater);
 
+
+	//Islands erzeugen
+	m_zs.AddPlacement(&m_zpIsland1);
+	m_zs.AddPlacement(&m_zpIsland2);
+	m_zs.AddPlacement(&m_zpIsland3);
+
+
+	m_zpIsland2.RotateY(THIRDPI);
+	m_zpIsland2.Translate(4000, 0, 3500);
+
+	m_zpIsland3.RotateY(-HALFPI);
+	m_zpIsland3.Translate(-5700, 0, -3500);
+
+
+	m_zpIsland1.AddGeo(&m_zgWater);
+
+
+	m_zpIsland1.AddPlacement(&m_zpHauptInsel);
+	m_zpIsland2.AddPlacement(&m_zpHauptInsel);
+	m_zpIsland3.AddPlacement(&m_zpHauptInsel);
+
+	m_pperlin = new CPerlin(PERLIN_SEED, 3.0f, 15, 0.5f, 8.0f, 1.50f, 1.5f, ePerlinInterpol_Standard, false);
+	m_pblobAllPositive = new CBlob(0.5f, 0.5f, 0.6f, 0.6f, -0.1f, eBlobShapeGround_Rect, eBlobShapeSide_All, NULL);
+	m_pblobAllNegative = new CBlob(0.5f, 0.5f, 0.6f, 0.6f, +0.1f, eBlobShapeGround_Rect, eBlobShapeSide_All, NULL);
+	m_pblobTerrainPositive = new CBlob(0.5f, 0.5f, 0.5f, 0.5f, TERRAIN_HEIGHT, eBlobShapeGround_Radial, eBlobShapeSide_Hill, m_pperlin);
+	m_pblobTerrainNegative = new CBlob(0.5f, 0.5f, 0.5f, 0.5f, -TERRAIN_HEIGHT, eBlobShapeGround_Radial, eBlobShapeSide_Hill, m_pperlin);
+	m_pblobTerrainCraterPositive = new CBlob(0.2f, 0.2f, 0.2f, 0.2f, TERRAIN_HEIGHT, eBlobShapeGround_Donut, eBlobShapeSide_Hill, m_pperlin);
+	m_pblobAll = new CBlob(0.5f, 0.5f, 0.51f, 0.51f, -0.2, eBlobShapeGround_Rect, eBlobShapeSide_All, nullptr);
+
+	m_zgTerrainOri.AddBlob(m_pblobAllPositive);
+	m_zgTerrainOri.AddBlob(m_pblobTerrainPositive);
+	m_zgTerrainOri.AddBlob(m_pblobTerrainCraterPositive);
+	m_zgTerrainOri.AddBlob(m_pblobAll);
+
+	m_zgTerrainOri.CreateField(TERRAIN_SIZE, TERRAIN_SIZE, 1000, 1000, 0.0f, 0.0f, 1.0f, 1.0f);
+
+
+	//
+	//Terrain mit verschiedenen Materialien erstellen
+
+	m_cutUnderSea.SetHeightLimits(-F_MAX, 0.0f);
+	m_cutUnderSea.SetFlattenLowerOn();
+	m_cutUnderSea.SetFlattenSmartOn();
+	m_cutUnderSea.SetFlattenUpperOn();
+	m_zgTerrain.AddCut(&m_cutUnderSea);
+
+	/*m_cutSeaToSand.SetHeightLimits(0.0f,TERRAIN_SANDLIMIT);
+	m_cutSeaToSand.SetHeightInverseOn();
+	m_cutSeaToSand.SetFlattenLowerOn();
+	m_cutSeaToSand.SetFlattenSmartOn();
+
+
+	m_cutSandtoBeach.SetHeightLimits(TERRAIN_SANDLIMIT, TERRAIN_BEACHLIMIT);
+	m_cutSandtoBeach.SetHeightInverseOn();
+
+	m_cutSeaToBeach.SetHeightLimits(0.0f, TERRAIN_BEACHLIMIT);
+	m_cutSeaToBeach.SetHeightInverseOn();
+	m_cutSeaToBeach.SetFlattenLowerOn();
+	m_cutSeaToBeach.SetFlattenSmartOn();*/
+	//
+	//
+
+
+	// Lade die Texturen für Wasser und Land:
+
+	m_zmRock.LoadPreset("Rock");//Laden der Steinstruktur
+	m_zmRock.SetHeightStrength(1.5);
+	m_zmSand.LoadPreset("Sand");
+	m_zmEarth.LoadPreset("RockMossy");
+
+
+
+	//Wasser
+	m_zmWater.LoadPreset("Water");//Laden des Wassers
+	m_zmWater.SetAni(8, 8, 7);	//Animierte Wassertextur hat 8x8 Bilder 
+	m_zmWater.Translate(CColor(0.8f, 0.0f, 0.0f)); //Blauwert vom Wasser erhöhen
+	m_zmWater.SetTransparency(0.8f);//Durchsichtigkeit des Wassers
+	m_zmWater.SetTransparencyOn();
+	m_zmWater.MakeTextureHaze("textures//WaterH.jpg");
+	m_zmWater.SetPostprocessingOn();
+	m_zmWater.SetHazeOn();
+	m_zmWater.SetHazeStrength(1.8);
+
+
+
+
+
+	// Erzeuge mit dem Terraindaten eine Terraingeometrie: 
+	m_zgTerrain.InitFromOther(m_zgTerrainOri, &m_zmEarth);
+
+
+	// Erzeuge die Wasseroberfläche:
+	m_zgWater.Init(WATER_SIZE, WATER_SIZE, &m_zmWater, 10, 10, 0.0f, 0.0f, 500.0f, 500.0f);
+
+
+
 	// Füge das Terrain dem Kollisionscontainer hinzu:
 	m_zgsTerrain.Add(&m_zgTerrain);
 
-	// -----------------------
-	// Kamera:
-	// -----------------------
 
-	// Initialisiere die Kamera mit Outdoor-BVH-Schattenfrustumcasting (OBVHSFC) zur Beschleunigung:
+	// Initialisiere die Kamera mit Outdoor-BVH-
+	// Schattenfrustumcasting (OBVHSFC) zur Beschleunigung:
 	m_zs.SetFrustumCullingOn();
-	m_zc.Init(HALFPI, // 45° Kameraöffnungswinkel
-		0.3, 170000.0f, // 30cm bis 170 km Sicht
-		true, // BVH-Schattenfrustumcasting an!
-		m_zs.GetSkyLightPlacement()); // Info für das OBVHSFC
+	m_zc.Init(HALFPI, 0.3, 170000.0f, true, m_zs.GetSkyLightPlacement());
 
 	// Initialisiere die Kamera:
 	m_zs.AddPlacement(&m_zpCamera);
 	m_zpCamera.AddCamera(&m_zc);
+	// Die Kamera soll sich 30 m/s bewegen:
+	m_zpCamera.SetTranslationSensitivity(1000);
+	// Stelle die Kamera an einen geeigneten Anfangsort: 
+	m_zpCamera.Translate(0.0f, 20.0f, 1000.0f);
 
-	// Die Kamera soll sich bewegen:
-	m_zpCamera.SetTranslationSensitivity(500);
-	m_zpCamera.SetRotationSensitivity(1.0f);
-
-	// Stelle die Kamera an einen geeigneten Anfangsort:
-	m_zpCamera.Translate(0.0f, 1000.0f, 1000.0f);
-
-
-	// Turret importieren
-	//m_zgTurret = m_fileWavefront.LoadGeoTriangleTable("C:\\Users\\y_sch\\Desktop\\Hochschule\\3 Semester\\Vektoria\\VektoriaV23.20\\LandscapeApp\\wavefront\\turret.obj");
-	m_zgTurret = m_fileWavefront.LoadGeoTriangleTable("C:\\Users\\y_sch\\Desktop\\Hochschule\\3 Semester\\Vektoria\\VektoriaV23.20\\LandscapeApp\\Models\\Star Wars emperor Turret.obj");
-
-	m_zs.AddPlacement(&m_zpTurret);
-	m_zpTurret.AddGeo(m_zgTurret);
-
-	m_zpTurret.ScaleDelta(50.0f);
-	/*m_zpTurret.GetPos().GetX;*/
-	float fH = m_zgTerrain.GetHeight(0.0f, 0.0f);
-
-	m_zpTurret.TranslateYDelta(fH);
-	m_zpTurret.RotateYDelta(-HALFPI);
-
-
-	/*
-
-	m_zgTerrain.SwitchCollisionGroundOn();
-
-	CHVector rayStart = m_zpTurret.GetPos();
-	CRay r(rayStart, CHVector(0.0f, -1.0f, 0.0f));
-	CHitPoint hitpoint;
-
-
-
-	m_zpTurret.IntersectsGeosGround(r, hitpoint, 1000000000, true);
-	m_zgTerrain.Intersects(r, hitpoint, 1000000000, true);
-	m_zgTerrainOriginal.Intersects(r, hitpoint, 1000000000 , true);
-	*/
-	//m_zpTurret.TranslateYDelta(-hitpoint.m_fDist);
-	
-
-
-	m_zgSphere.Init(25.0f, NULL, 100, 100);
-	m_zs.AddPlacement(&m_zpSphere);
-	m_zpSphere.AddGeo(&m_zgSphere);
-	m_zpSphere.TranslateY(800.0f);
 }
 
 void CGame::Tick(float fTime, float fTimeDelta)
@@ -188,11 +187,13 @@ void CGame::Tick(float fTime, float fTimeDelta)
 	//-------------------------------------------
 
 	// Hier kommen die Veränderungen pro Renderschritt hinein: 
-	
-	m_zdk.PlaceWASD(m_zpCamera, fTimeDelta, true);
+
+
 
 	// Traversiere am Schluss den Szenengraf und rendere:
+	m_zdk.PlaceWASD(m_zpCamera, fTimeDelta, true);
 	m_zr.Tick(fTimeDelta);
+
 }
 
 void CGame::Fini()
@@ -205,6 +206,9 @@ void CGame::Fini()
 
 	// Auch die Root sollte finalisiert werden:   
 	m_zr.Fini();
+	delete m_pperlin;
+	delete m_pblob;
+
 }
 
 void CGame::WindowReSize(int iNewWidth, int iNewHeight)
@@ -223,5 +227,7 @@ float CGame::GetVersion()
 {
 	return m_zr.GetVersion();
 }
+
+
 
 
--- LandscapeApp/VektoriaApp/Game.h
+++ LandscapeApp/VektoriaApp/Game.h
@@ -1,4 +1,5 @@
 #pragma once
+
 //-----------------------------------------------------------------------
 // CGame
 // 
@@ -11,7 +12,12 @@
 // Copyright : Tobias Breiner  
 // Disclaimer: Nutzung auf eigene Gefahr, keine Gewähr, no warranty!
 //------------------------------------------------------------------------
-
+#define TERRAIN_HEIGHT 500
+#define TERRAIN_SIZE 2500
+#define WATER_SIZE 20000
+#define TERRAIN_BEACHLIMIT 12
+#define TERRAIN_SANDLIMIT 4
+#define PERLIN_SEED 42
 
 #ifdef _WIN64
 #ifdef _DEBUG
@@ -41,25 +47,25 @@ class CGame
 {
 public:
 	// Wird vor Begin einmal aufgerufen (Konstruktor):
-	CGame(void);																				
+	CGame(void);
 
 	// Wird nach Ende einmal aufgerufen (Destruktor):
-	~CGame(void);																				
+	~CGame(void);
 
 	// Wird zu Begin einmal aufgerufen:
-	void Init(HWND hwnd, void(*procOS)(HWND hwnd, unsigned int uWndFlags), CSplash * psplash);	
+	void Init(HWND hwnd, void(*procOS)(HWND hwnd, unsigned int uWndFlags), CSplash* psplash);
 
 	// Wird während der Laufzeit bei jedem Bildaufbau aufgerufen:
-	void Tick(float fTime, float fTimeDelta);						
+	void Tick(float fTime, float fTimeDelta);
 
 	// Wird am Ende einmal aufgerufen:
-	void Fini();																				
+	void Fini();
 
 	// Wird immer dann aufgerufen, wenn der Benutzer die Fenstergröße verändert hat:
-	void WindowReSize(int iNewWidth, int iNewHeight);											
+	void WindowReSize(int iNewWidth, int iNewHeight);
 
 	// Holt das minimale Zeitdelta zur eventuellen Frame-Rate-Beschränkung:
-	float GetTimeDeltaMin();																	
+	float GetTimeDeltaMin();
 
 	// Holt die Versionsnummer:
 	float GetVersion();
@@ -68,40 +74,98 @@ public:
 private:
 
 	// Die Wurzel des Szenengrafen: 
-	CRoot m_zr; 
-
-	// Hier ist Platz für Deine weiteren Vektoriaobjekte:
+	CRoot m_zr;
 	CScene m_zs;
 	CFrame m_zf;
 	CViewport m_zv;
 	CCamera m_zc;
 	CDeviceKeyboard m_zdk;
 	CPlacement m_zpCamera;
-	CGeoTerrain m_zgTerrainOriginal; // Gesamtes Terrain
+	CGeoTerrain m_zgTerrainOri; // Gesamtes Terrain
 	CGeoTerrain m_zgTerrain; // Inselausschnitt
-	CGeoTerrain m_zgWater; // Wasserausschnitt
+	CGeoTerrain m_zgWater;// Wasserausschnitt
+
+
+
+	CMaterial m_zmRock;
+	CMaterial m_zmSnow;
+	CMaterial m_zmSand;
+
+
+
 	CPlacement m_zpLandscape; // Insel- & Wasserplacement
-
 	CPerlin* m_pperlin = nullptr;// Perlin Noise
+	CPerlin* m_pperlin1 = nullptr;
 
-	CBlob* m_pblob = nullptr; // Erhebung aus dem Meer
-	CBlob* m_pblob2 = nullptr; // Erhebung aus dem Meer
-	CBlob* m_pblob3 = nullptr; // Erhebung aus dem Meer
-	CBlob* m_pblob4 = nullptr; // Erhebung aus dem Meer
-	CBlob* m_pblob5 = nullptr; // Erhebung aus dem Meer
-	
+
+
+
+	//Placements
+
+	CFileWavefront m_filewavefront;
+	CGeoTriangleTable* m_pgtriangletable = nullptr;
+	CPlacement m_zpCannon;
+
+
+
+
+	CFileWavefront m_filewavefront1;
+	CGeoTriangleTable* m_pgtriangletable1 = nullptr;
+	CPlacement m_zpShip;
+
+
+
+	//CGeoTerrain m_zgTerrainMirror;
+	//CGeoTerrain m_zgTerrainMirrorLOD1;
+
+
+
+
+	//Insel Test
+	CPlacement m_zpHauptInsel;
+	CPlacement m_zpIsland1;
+	CPlacement m_zpIsland2;
+	CPlacement m_zpIsland3;
+	CBlob* m_pblobAllPositive = nullptr;
+	CBlob* m_pblobAllNegative = nullptr;
+	CBlob* m_pblobTerrainPositive = nullptr;
+	CBlob* m_pblobTerrainNegative = nullptr;
+	CBlob* m_pblobTerrainCraterPositive = nullptr;
+
+
+	CBlob* m_pblob9 = nullptr;
+	CBlob* m_pblob8 = nullptr;
+	CBlob* m_pblob6 = nullptr;
+	CBlob* m_pblob7 = nullptr;
+	CBlob* m_pblob5 = nullptr;
+	CBlob* m_pblob3 = nullptr;
+	CBlob* m_pblob4 = nullptr;
+	CBlob* m_pblob2 = nullptr;
+	CBlob* m_pblob1 = nullptr;
+	CBlob* m_pblob = nullptr;
+	CBlob* m_pblobAll = nullptr;
+
+
+	// Erhebung aus dem Meer
 	CCut m_cutUnderSea; // Schnitt unter N.N.
 	CMaterial m_zmWater; // Wellenmaterial
 	CMaterial m_zmEarth; // Erdmaterial
 	CGeos m_zgsCollision, m_zgsHeight; // Kollisionscontainer
 	CGeoTerrains m_zgsTerrain; // Terrainkollisionscontainer
 
-	CFileWavefront m_fileWavefront;
-	CGeoTriangleTable* m_zgTurret = nullptr;
-	CPlacement m_zpTurret;
 
-	CGeoSphere m_zgSphere;
-	CPlacement m_zpSphere;
+
+
+	CCut m_cutOverSea;
+	CCut m_cutSeaToSand;
+	CCut m_cutSandtoBeach;
+	CCut m_cutSeaToBeach;
+
+
+
+	// Hier ist Platz für Deine weiteren Vektoriaobjekte:
+
+
 };
 
 
