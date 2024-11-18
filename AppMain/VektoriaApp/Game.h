1e05dc9c1afbcf8cab62243d3ff608de445f0664
Rim3Xx <fweiske@gmx.de>
2024-11-18 14:33:43 +0100

    neues Terrain mit viel liebe
    neues Terrain mit viel liebe

--- AppMain/VektoriaApp/Game.h
+++ AppMain/VektoriaApp/Game.h
@@ -17,6 +17,9 @@
 #define WATER_SIZE 20000
 #define TERRAIN_BEACHLIMIT 12
 #define TERRAIN_SANDLIMIT 4
+#define TERRAIN_SNOWLIMIT 130
+#define TERRAIN_SNOWLIMITHEAVY 150
+#define TERRAIN_UV 50
 #define PERLIN_SEED 42
 #define TURRET_COUNT 6
 
@@ -115,6 +118,13 @@ private:
 	CDeviceGameController m_zdgc;
 	CDeviceKeyboard m_zdk;
 
+	///////////////////////////////////////////////////
+	//////////////////// CROSSHAIR ////////////////////
+	///////////////////////////////////////////////////
+	CImage m_ziCrosshair; CImage m_ziCirclehair;
+	COverlay m_zoCrosshair; COverlay m_zoCirclehair;
+	C2dRect m_zCrosshairRect;
+
 
 	///////////////////////////////////////////////////
 	//////////////////// TERRAIN //////////////////////
@@ -126,6 +136,11 @@ private:
 	CMaterial m_zmRock;
 	CMaterial m_zmSnow;
 	CMaterial m_zmSand;
+	CMaterial m_zmSnowHeavy;
+	CMaterial m_zmSandMossy;
+	CMaterial m_zmMirror;
+	CMaterial m_zmWater; // Wellenmaterial
+	CMaterial m_zmEarth; // Erdmaterial
 
 	CPlacement m_zpLandscape; // Insel- & Wasserplacement
 	CPerlin* m_pperlin = nullptr;// Perlin Noise
@@ -167,19 +182,6 @@ private:
 	CMaterial m_zmBullet;
 
 
-	///////////////////////////////////////////////////
-	//////////////////// CROSSHAIR ////////////////////
-	///////////////////////////////////////////////////
-	CImage m_ziCrosshair; CImage m_ziCirclehair;
-	COverlay m_zoCrosshair; COverlay m_zoCirclehair;
-	C2dRect m_zCrosshairRect;
-
-	//CGeoTerrain m_zgTerrainMirror;
-	//CGeoTerrain m_zgTerrainMirrorLOD1;
-
-
-
-
 	//Insel Test
 	CPlacement m_zpHauptInsel;
 	CPlacement m_zpIsland1;
@@ -206,9 +208,6 @@ private:
 
 
 	// Erhebung aus dem Meer
-	CCut m_cutUnderSea; // Schnitt unter N.N.
-	CMaterial m_zmWater; // Wellenmaterial
-	CMaterial m_zmEarth; // Erdmaterial
 	CGeos m_zgsCollision, m_zgsHeight; // Kollisionscontainer
 	CGeoTerrains m_zgsTerrain; // Terrainkollisionscontainer
 
@@ -219,7 +218,28 @@ private:
 	CCut m_cutSeaToSand;
 	CCut m_cutSandtoBeach;
 	CCut m_cutSeaToBeach;
+	CCut m_cutUnderSea; // Schnitt unter N.N.
+	CCut m_cutBeachToSnowLimit;
+	CCut m_cutSnowLimitToHeavySnowLimit;
+	CCut m_cutOverHeavySnowLimit;
+	CCut m_cutOverSnowLimit;
 
+	CCut m_cutUnder15Degrees;
+	CCut m_cut15DegreesTo30Degrees;
+	CCut m_cut30DegreesTo45Degrees;
+	CCut m_cut15DegreesTo45Degrees;
+	CCut m_cutOver45Degrees;
+	CCut m_cutUnder45Degrees;
+
+
+	CGeoTerrain m_zgTerrainFlora;
+	CGeoTerrain m_zgTerrainLow;
+	CGeoTerrain m_zgTerrainMirror;
+	CGeoTerrain m_zgTerrainRock;
+	CGeoTerrain m_zgTerrainSnow;
+	CGeoTerrain m_zgTerrainSand;
+	CGeoTerrain m_zgTerrainSandMossy;
+	CGeoTerrain m_zgTerrainSnowHeavy;
 
 
 	//Overlay
